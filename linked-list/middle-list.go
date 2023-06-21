13d7cd5ddbb1924fb8c19c1531c89c59b5a549d6
edgarSucre <edgar.sucre.02@gmail.com>
2023-06-21 18:36:52 -0500

    added soulution for middle-list

--- linked-list/middle-list.go
+++ linked-list/middle-list.go
@@ -1,6 +1,13 @@
 package linklist
 
 func MiddleNode[T any](h *ListNode[T]) (middle *ListNode[T]) {
+	middle = h
+	end := h
+
+	for end != nil && end.Next != nil {
+		middle = middle.Next
+		end = end.Next.Next
+	}
 
 	return middle
 }
