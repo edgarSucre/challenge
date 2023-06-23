88036402e9b947c04b92ad6f30e2a0bf6d654c13
edgarSucre <edgar.sucre.02@gmail.com>
2023-06-23 09:29:34 -0500

    added palindrome solution

--- array/palindrome.go
+++ array/palindrome.go
@@ -1,6 +1,16 @@
 package array
 
 func IsPalindrome(n int) bool {
+	if n < 0 {
+		return false
+	}
 
-	return true
+	var reversed int
+	x := n
+	for x != 0 {
+		reversed = reversed*10 + x%10
+		x /= 10
+	}
+
+	return reversed == n
 }
