0dc444b8ef2c12922096c5c54f51724d567e2152
edgarSucre <edgar.sucre.02@gmail.com>
2023-06-24 07:31:30 -0500

    added solution fro roman to int

--- array/roma-to-int.go
+++ array/roma-to-int.go
@@ -1,5 +1,27 @@
 package array
 
 func RomanToInt(s string) int {
-	return 0
+	guide := map[byte]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+
+	var n, previous int
+
+	for i := len(s) - 1; i >= 0; i-- {
+		current := guide[s[i]]
+		if current < previous {
+			n -= current
+		} else {
+			n += current
+		}
+		previous = current
+	}
+
+	return n
 }
