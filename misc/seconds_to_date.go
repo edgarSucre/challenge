5eb232eb30db530b56a220e7b3febdc447c8463e
edgarSucre <edgar.sucre.02@gmail.com>
2025-05-27 08:31:01 -0500

    added solution for secondsToDate

--- misc/seconds_to_date.go
+++ misc/seconds_to_date.go
@@ -1,5 +1,47 @@
 package misc
 
+import (
+	"fmt"
+	"strings"
+)
+
 func SecondsToDate(seconds int) string {
-	return ""
+	const (
+		minute = 60
+		hour   = 3600
+		day    = 86400
+		year   = 31536000
+	)
+
+	var sol string
+
+	if seconds >= year {
+		years := seconds / year
+		sol = fmt.Sprintf("%s %v years,", sol, years)
+
+		seconds = seconds % year
+	}
+
+	if len(sol) > 0 || seconds >= day {
+		days := seconds / day
+		sol = fmt.Sprintf("%s %v days,", sol, days)
+
+		seconds = seconds % day
+	}
+
+	if len(sol) > 0 || seconds >= hour {
+		hours := seconds / hour
+		sol = fmt.Sprintf("%s %v hours,", sol, hours)
+
+		seconds = seconds % hour
+	}
+
+	if len(sol) > 0 || seconds >= minute {
+		minutes := seconds / minute
+		sol = fmt.Sprintf("%s %v minutes,", sol, minutes)
+
+		seconds = seconds % minute
+	}
+
+	return strings.TrimSpace(fmt.Sprintf("%s %v seconds", sol, seconds))
 }
